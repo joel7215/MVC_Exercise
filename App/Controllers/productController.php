d74cf8625c4d736e41f3c4708bbc324e1cf9f931
joel7215 <joel.estragues@cirvianum.cat>
2025-10-19 19:24:35 +0200

    user

--- App/Controllers/productController.php
+++ App/Controllers/productController.php
@@ -3,3 +3,15 @@
 //cal crear la classe productController
 //el metode per defecte "index" ha de cridar la vista
 //product.view.php
+
+use App\Core\Controller;
+use App\Models\User;
+use App\Models\Orm;
+
+class productController extends Controller
+{
+	public function index()
+	{
+		$this->render("user/product");
+	}
+}
